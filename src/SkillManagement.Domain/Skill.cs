0ba96c81b62303c9a6689f0c2de8a56bc5298789
Hermogenes Ferreira <ferreira.hermogenes@gmail.com>
2021-10-08 12:01:41 +0200

    feat: Create domain

---
+++ src/SkillManagement.Domain/Skill.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SkillManagement.Domain
+{
+    public sealed class Skill
+    {
+        public string Title { get; }
+        public string Description { get; }
+
+        public Skill(string title, string description)
+        {
+            Title = title ?? throw new ArgumentNullException(nameof(title));
+
+            if (string.IsNullOrWhiteSpace(title)) throw new ArgumentNullException(nameof(title));
+            
+            Description = description;
+        }
+    }
+}
---
+++ test/SkillManagement.Domain.Tests/SkillTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using AutoFixture.Xunit2;
+using FluentAssertions;
+using Xunit;
+
+namespace SkillManagement.Domain.Tests
+{
+    public class SkillTests
+    {
+        [Theory]
+        [AutoData]
+        public void GivenTitleIsNull_ThrowsArgumentNullException(string anyDescription)
+        {
+            Action action = () => _ = new Skill(null, anyDescription);
+
+            action.Should().ThrowExactly<ArgumentNullException>().And.ParamName.Should().Be("title");
+        }
+
+        [Theory]
+        [AutoData]
+        public void GivenTitleIsEmpty_ThrowsArgumentNullException(string anyDescription)
+        {
+            Action action = () => _ = new Skill(string.Empty, anyDescription);
+
+            action.Should().ThrowExactly<ArgumentNullException>().And.ParamName.Should().Be("title");
+        }
+
+        [Theory]
+        [AutoData]
+        public void GivenValidTitleAndDescription_NewObjectIsCreatedSuccessfully(string title, string description)
+        {
+            var skill = new Skill(title, description);
+
+            skill.Title.Should().Be(title);
+            skill.Description.Should().Be(description);
+        }
+    }
+}
