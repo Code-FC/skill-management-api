55632756ddf463d1e5e52b67c27317a851edc86a
Hermogenes Ferreira <ferreira.hermogenes@gmail.com>
2021-10-15 08:33:58 +0200

    feat: Change Skill to be comparable and have an Id

--- src/SkillManagement.Domain/Skill.cs
+++ src/SkillManagement.Domain/Skill.cs
@@ -4,19 +4,42 @@ namespace SkillManagement.Domain
 {
     public sealed class Skill
     {
-        public Skill(string title, string description)
+        public Skill(string title, string description) : this(Guid.NewGuid(), title, description)
         {
-            Title = title ?? throw new ArgumentNullException(nameof(title));
+        }
 
-            if (string.IsNullOrWhiteSpace(title))
-            {
-                throw new ArgumentNullException(nameof(title));
-            }
-
+        public Skill(Guid id, string title, string description)
+        {
+            Id = id == default ? throw new ArgumentNullException(nameof(id)) : id;
+            Title = string.IsNullOrWhiteSpace(title) ? throw new ArgumentNullException(nameof(title)) : title;
             Description = description;
         }
 
+        public Guid Id { get; }
         public string Title { get; }
         public string Description { get; }
+
+        public override bool Equals(object? obj)
+        {
+            return ReferenceEquals(this, obj) ||
+                   (obj is Skill other &&
+                    (Id.Equals(other.Id) ||
+                     string.Equals(Title, other.Title, StringComparison.InvariantCultureIgnoreCase)));
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public static bool operator ==(Skill? left, Skill? right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Skill? left, Skill? right)
+        {
+            return !Equals(left, right);
+        }
     }
 }
