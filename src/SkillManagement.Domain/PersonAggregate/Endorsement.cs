7efc2393137993af4d49b51e336ba0968a2aba4c
Hermogenes Ferreira <ferreira.hermogenes@gmail.com>
2021-10-15 13:57:26 +0200

    feat: Adjust Person domain to include skills and endorsements

--- src/SkillManagement.Domain/PersonAggregate/Endorsement.cs
+++ src/SkillManagement.Domain/PersonAggregate/Endorsement.cs
@@ -1,19 +1,17 @@
 ﻿using System;
 
-namespace SkillManagement.Domain
+namespace SkillManagement.Domain.PersonAggregate
 {
-    public class Endorsement
+    public sealed class Endorsement
     {
-        public Endorsement(Person endorser, Person endorsee, Skill skill, string comment)
+        internal Endorsement(Person endorser, Skill skill, string comment)
         {
             Endorser = endorser ?? throw new ArgumentNullException(nameof(endorser));
-            Endorsee = endorsee ?? throw new ArgumentNullException(nameof(endorsee));
             Skill = skill ?? throw new ArgumentNullException(nameof(skill));
             Comment = comment ?? throw new ArgumentNullException(nameof(comment));
         }
 
         public Person Endorser { get; }
-        public Person Endorsee { get; }
         public Skill Skill { get; }
         public string Comment { get; }
     }
---
+++ src/SkillManagement.Domain/PersonAggregate/EvaluationLevel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SkillManagement.Domain
+{
+    public enum EvaluationLevel
+    {
+        NotEvaluated = 0,
+        Level1 = 1,
+        Level2 = 2,
+        Level3 = 3,
+        Level4 = 4,
+        Level5 = 5,
+    }
+}
