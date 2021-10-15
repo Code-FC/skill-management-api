4e4f44b4d0fb4b93028356d280d99da1dab5a37b
Hermógenes Ferreira <ferreira.hermogenes@gmail.com>
2021-10-15 09:18:32 +0200

    feat: Change Skill to be comparable and have an Id (#7)

--- test/SkillManagement.Domain.Tests/SkillTests.cs
+++ test/SkillManagement.Domain.Tests/SkillTests.cs
@@ -27,12 +27,64 @@ namespace SkillManagement.Domain.Tests
 
         [Theory]
         [AutoData]
-        public void GivenValidTitleAndDescription_NewObjectIsCreatedSuccessfully(string title, string description)
+        public void GivenIdIsDefault_ThrowsArgumentNullException(string anyTitle, string anyDescription)
+        {
+            Action action = () => _ = new Skill(Guid.Empty, anyTitle, anyDescription);
+
+            action.Should().ThrowExactly<ArgumentNullException>().And.ParamName.Should().Be("id");
+        }
+
+        [Theory]
+        [AutoData]
+        public void GivenValidTitleAndDescription_NewObjectIsCreatedSuccessfullyWithGeneratedId(string title,
+            string description)
         {
             var skill = new Skill(title, description);
 
+            skill.Id.Should().NotBe(default(Guid));
             skill.Title.Should().Be(title);
             skill.Description.Should().Be(description);
+        }
+
+        [Theory]
+        [AutoData]
+        public void GivenValidIdAndTitleAndDescription_NewObjectIsCreatedSuccessfully(Guid id, string title,
+            string description)
+        {
+            var skill = new Skill(id, title, description);
+
+            skill.Id.Should().Be(id);
+            skill.Title.Should().Be(title);
+            skill.Description.Should().Be(description);
+        }
+
+        [Theory, AutoData]
+        public void GivenTwoSkillObjectsWithSameId_TheyAreConsideredEqual(Guid id,
+            string titleLeft,
+            string descriptionLeft,
+            string titleRight,
+            string descriptionRight)
+        {
+            var left = new Skill(id, titleLeft, descriptionLeft);
+            var right = new Skill(id, titleRight, descriptionRight);
+
+            left.Equals(right).Should().Be(true);
+        }
+
+        [Theory, AutoData]
+        public void GivenTwoSkillObjectsWithSameTitle_TheyAreConsideredEqual(
+            string title,
+            string descriptionLeft,
+            string descriptionRight)
+        {
+            var left = new Skill(title, descriptionLeft);
+            var right = new Skill(title, descriptionRight);
+
+            left.Equals(right).Should().BeTrue();
+            (left == right).Should().BeTrue();
+            (left != right).Should().BeFalse();
+
+            left.GetHashCode().Should().Be(left.Id.GetHashCode());
         }
     }
 }
